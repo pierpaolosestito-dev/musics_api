4066960ae32ead669e4b56273d9f055cccbed913
pierpaolosestito200933 <sstppl99h29d122x@studenti.unical.it>
2022-11-24 15:25:05 +0100

    validators 'n' model

--- musics/models.py
+++ musics/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
+
+#CD:#
+#- Nome ->
+#- Band/Artista
+#- Casa discografica
+# - Categoria
+# - Codice(UPC/EAN)
+# - Prezzo
+# - Pubblicato_da
 # Create your models here.
+class CD(models.Model):
+    name = models.CharField(max_length=50) #TODO Validators
+    band = models.CharField(max_length=50) #TODO Validators
+
+
+
