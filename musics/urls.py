befd26f60df0b36700bc48204ea21b341d34aa8d
pierpaolosestito200933 <sstppl99h29d122x@studenti.unical.it>
2022-11-26 12:23:40 +0100

    added missing files in repo

---
+++ musics/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+from rest_framework.routers import SimpleRouter
+
+from musics.views import CDViewSet
+
+router = SimpleRouter()
+router.register('',CDViewSet,basename="musics")
+
+urlpatterns =  router.urls
